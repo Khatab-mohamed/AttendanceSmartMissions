a399588c8dd687f7486972878c67bf244f342888
Khatab <khatab.mohammed@mdd.sa>
2024-01-15 12:59:49 -0900

    Edit Program

--- AMS.Api/Program.cs
+++ AMS.Api/Program.cs
@@ -52,14 +52,6 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()&& app.Environment.IsProduction())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseCors(corsOpenPolicy);
-}
-
 app.Map("/error", (HttpContext httpContext) =>
 {
     var exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
